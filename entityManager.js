46460a08884b4e2aaa5436456bd7430c9371c8ce
Espen Storfjell <64396357+EStorfjell@users.noreply.github.com>
2021-01-28 14:17:39 -0800

    Created file to structure map walls and implemented bounding boxes

--- entityManager.js
+++ entityManager.js
@@ -8,8 +8,10 @@ class EntityManager {
     };
 
     loadLevelOne() {
-        let map = new Map(gameEngine, "./maps/Map1.png");
+        let map = new Map(gameEngine, LevelOne);
         this.gameEngine.addEntity(map);
+        map.init(gameEngine);
+
         let zombie = new Zombie(this.gameEngine, this.hero, 400, 100);
         this.gameEngine.addEntity(zombie);
         let skeleton = new Skeleton(this.gameEngine, this.hero, 100, 200);
@@ -22,9 +24,17 @@ class EntityManager {
 }
 
 class Map {
-    constructor(game, imgPath) {
-        this.game = game;
-        this.image = ASSET_MANAGER.getAsset(imgPath);
+    constructor(game, level) {
+        Object.assign(this, {game, level});
+        this.image = ASSET_MANAGER.getAsset(level.imgPath);
+    };
+
+    init(game) {
+        let that = this;
+        this.level.walls.forEach(function (item) {
+            let wall = new Wall(that.game, item.x, item.y, item.width, item.height);
+            that.game.addEntity(wall);
+        });
     };
 
     update() {
@@ -33,5 +43,25 @@ class Map {
 
     draw(ctx) {
         ctx.drawImage(this.image, 0, 0, 640, 480);
+    };
+}
+
+class Wall {
+    constructor(game, x, y, width, height) {
+        Object.assign(this, {game, x, y, width, height});
+        this.updateBB();
+    };
+
+    update() {
+        this.updateBB();
+    };
+
+    draw(ctx) {
+
+    };
+
+    updateBB() {
+        this.lastBB = this.BB;
+        this.BB = new BoundingBox(this.x, this.y, this.width, this.height);
     };
 }
