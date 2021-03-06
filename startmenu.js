ff10a6e1b8e5498c463b312356647f94190d3e2d
Kyle Oslin <oslinkj@uw.edu>
2021-03-05 16:51:17 -0800

    Added title to the start menu

--- startmenu.js
+++ startmenu.js
@@ -14,7 +14,13 @@ class StartMenu {
         ctx.fillStyle = "wheat";
         ctx.fillRect(0, 0, this.game.surfaceWidth, this.game.surfaceHeight);
 
-        ctx.font = "72px serif";
+        ctx.font = '60px "Felipa"';
+        ctx.textAlign = "center";
+        ctx.textBaseline = "top";
+        ctx.fillStyle = "black";
+        ctx.fillText("Ye Olde Onslaught", this.game.surfaceWidth / 2, 15);
+
+        ctx.font = '45px "Noto Serif"';
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
         ctx.fillStyle = "black";
